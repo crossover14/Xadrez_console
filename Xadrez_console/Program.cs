af8fd2ee502e34e5fe3002ffcf6812e533243c5a
Leandro Vieira <leandro.v.pordeus@gmail.com>
2022-02-28 14:39:50 -0300

    criando classe PosicaoXadrez

--- Xadrez_console/Program.cs
+++ Xadrez_console/Program.cs
@@ -9,21 +9,9 @@ namespace Xadrez_console
         static void Main(string[] args)
         {
 
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 2));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 2));
-
-                Tela.ImprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('d', 8);
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
           
             Console.ReadLine();
 
